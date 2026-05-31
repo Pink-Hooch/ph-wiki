9cd2f5ac64d18701b0cfc021b2924fa72bcf418a
Adrian Joseph Kilgour <adriankilgourpc@gmail.com>
2026-05-31 03:32:28 +0100

    Remove GitHub Pages deployment configuration from docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,11 +25,6 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'ph-wiki', // Usually your repo name.
-
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
